337fb02625249454dc1d864854054bf192500e1e
neverUsedGithub <81071335+neverUsedGithub@users.noreply.github.com>
2024-02-10 11:42:49 +0100

    fix browser support

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { readFile } from "fs/promises";
 import { Lexer } from "./lexer";
 import { Parser } from "./parser";
 import { Transpiler } from "./transpiler";
@@ -11,6 +10,7 @@ export function transpileString(source: string): string {
 }
 
 export async function transpileFile(filename: string): Promise<string> {
+    const { readFile } = await import("fs/promises");
     const source = await readFile(filename, { encoding: "utf-8" });
     const tokens = new Lexer(source).lexAll();
     const ast = new Parser(tokens).parse();
