5855cd25f1597f8633bdd0b93019e904d7f350f2
neverUsedGithub <81071335+neverUsedGithub@users.noreply.github.com>
2024-02-17 21:44:19 +0100

    fixes

--- src/app.tsx
+++ src/app.tsx
@@ -3,13 +3,13 @@ import "./app.scss";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./pages/_root.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./pages/404";
 import IndexPage from "./pages/index";
 import PlaygroundPage from "./pages/Playground";
 import SyntaxReferencePage from "./pages/SyntaxReference";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     element: <App />,
     errorElement: <ErrorPage />,
@@ -19,9 +19,7 @@ const router = createBrowserRouter([
       { path: "/syntax", element: <SyntaxReferencePage /> },
     ],
   },
-], {
-  basename: "/KaLang/"
-});
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
