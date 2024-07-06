509b999ddc96142764e6265846d5bd9e3cd815ea
neverUsedGithub <81071335+neverUsedGithub@users.noreply.github.com>
2024-07-07 00:16:19 +0200

    switch to esm only

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     entry: ["./src/*"],
-    format: ["cjs", "esm"],
+    format: "esm",
     dts: true,
     clean: true,
-    bundle: false
+    bundle: false,
 });
