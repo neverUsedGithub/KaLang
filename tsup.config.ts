2777c78a31f27c33c045746fa2901f62f010996d
neverUsedGithub <81071335+neverUsedGithub@users.noreply.github.com>
2024-07-10 13:16:14 +0200

    fix nodejs compat

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,4 +6,18 @@ export default defineConfig({
     dts: true,
     clean: true,
     bundle: false,
+    plugins: [
+        {
+            name: "add-js",
+            renderChunk(code) {
+                return {
+                    code: code.replace(/^(import .*?) from "(.*?)";$/gm, (m, g0, g1) => {
+                        if (!g1.startsWith("./") || g1.endsWith(".json")) return `${g0} from "${g1}";`;
+
+                        return `${g0} from "${g1}.js";`;
+                    }),
+                };
+            },
+        },
+    ],
 });
