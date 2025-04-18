e338fd2199529bbe82ad3ee7e55b89c057ffabe3
neverUsedGithub <81071335+neverUsedGithub@users.noreply.github.com>
2025-04-19 00:52:09 +0200

    fix builds

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,19 +5,5 @@ export default defineConfig({
     format: "esm",
     dts: true,
     clean: true,
-    bundle: false,
-    plugins: [
-        {
-            name: "add-js",
-            renderChunk(code) {
-                return {
-                    code: code.replace(/^(import .*?) from "(.*?)";$/gm, (m, g0, g1) => {
-                        if (!g1.startsWith("./") || g1.endsWith(".json")) return `${g0} from "${g1}";`;
-
-                        return `${g0} from "${g1}.js";`;
-                    }),
-                };
-            },
-        },
-    ],
+    bundle: true
 });
